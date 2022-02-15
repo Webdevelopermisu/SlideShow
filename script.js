fdf2687d0599b8b4c561f61fbfbe95a6e49e381c
M_H_MISU <83594748+Webdevelopermisu@users.noreply.github.com>
2022-02-15 16:53:10 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let slideshowElement = document.querySelectorAll(".slideshow-element");
+let countElement = 1;
+setInterval(() => {
+    countElement++
+    let currentElement = document.querySelector(".current");
+
+    currentElement.classList.remove("current")
+    if (countElement > slideshowElement.length) {
+        slideshowElement[0].classList.add("current")
+        countElement = 1;
+    } else {
+        currentElement.nextElementSibling.classList.add("current")
+    }
+    
+}, 3000);
